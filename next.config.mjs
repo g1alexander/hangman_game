5e53360b1cbe1024f786eba8d5892dab2ce0fc66
g1alexander <alexander.granados.dev@gmail.com>
2024-04-13 09:59:03 -0500

    chore: config cors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  headers: [
+    {
+      source: "/api/:path*",
+      headers: [
+        { key: "Access-Control-Allow-Origin", value: "https://www.google.com" },
+        {
+          key: "Access-Control-Allow-Methods",
+          value: "GET, POST, PUT, DELETE, OPTIONS",
+        },
+        {
+          key: "Access-Control-Allow-Headers",
+          value: "X-Requested-With, Content-Type, Authorization",
+        },
+        { key: "Access-Control-Allow-Credentials", value: "true" },
+      ],
+    },
+  ],
+};
 
 export default nextConfig;
